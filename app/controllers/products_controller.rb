73677e368451ce4ed6b25c5bc8ef6d56da478ee5
Laura <laurao148@gmail.com>
2019-05-22 16:33:08 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,11 @@ class ProductsController < ApplicationController
 
   def add
     @cart = cart
-<<<<<<< HEAD
+
     @cart << params[:product]
-=======
+
     @cart << params[:item]
->>>>>>> 5bdbc02a2fc0702e271203e19122cdecefb33676
+
     render :index
   end
 
